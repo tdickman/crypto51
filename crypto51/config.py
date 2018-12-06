4b01e72d04b3a4d66e35475570645c5bcfb548ba
Steve <31445407+sia-steve@users.noreply.github.com>
2018-12-06 13:14:50 -0500

    Update config.py – Remove Siacoin
    Siacoin was recently forked, and your calculations use Nicehash which is resulting in a $0 1 hour attack cost. This is because Nicehash is not properly calculating how much hashrate they have for Siacoin, or they are and the actual number is 0.

--- crypto51/config.py
+++ crypto51/config.py
@@ -1,1 +1,1 @@
-coin_blacklist = set(['XDN', 'PPC', 'DGB', 'KMD', 'DCY', 'DP', 'NLG', 'DOGE', 'UNO', 'VIA', 'UBQ', 'GEO', 'XMY', 'FLO', 'FTC', 'PINK', 'CRW'])
+coin_blacklist = set(['XDN', 'PPC', 'DGB', 'KMD', 'DCY', 'DP', 'NLG', 'DOGE', 'UNO', 'VIA', 'UBQ', 'GEO', 'XMY', 'FLO', 'FTC', 'PINK', 'CRW', 'SC'])
