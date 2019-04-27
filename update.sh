e92a0e8d0171481f62f101b8e5600ea2b13f44cd
Tom Dickman <tdickman@gmail.com>
2019-04-27 12:17:24 -0500

    Add new api subdomain with cors enabled

--- update.sh
+++ update.sh
@@ -10,4 +10,7 @@ if [ "$1" != "noprompt" ]; then
     read -p "Please check the output and then press [ENTER] to continue"
 fi
 # gsutil defacl set public-read gs://www.crypto51.app
+# gsutil defacl set public-read gs://api.crypto51.app
 gsutil -h "Cache-Control:public, max-age=600" -m rsync -d -r dist/ gs://www.crypto51.app/
+gsutil -h "Cache-Control:public, max-age=600" cp dist/coins.json gs://api.crypto51.app/
+gsutil -h "Cache-Control:public, max-age=600" cp dist/coins.json gs://api.crypto51.app/history/`date +%Y/%m/%d/%H-%M`.json
